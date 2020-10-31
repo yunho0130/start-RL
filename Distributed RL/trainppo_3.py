39ed83b9a4d41e46973a9271aa57584691f2f574
Kyu Nam Cho <mystous@naver.com>
2020-10-31 23:18:16 +0900

    Create trainppo_3.py

---
+++ Distributed RL/trainppo_3.py
@@ -0,0 +1,6 @@
+from ray import tune
+from ray.rllib.agents.ppo import PPOTrainer
+import ray
+
+ray.init(address="auto")
+tune.run(PPOTrainer, stop={"episode_reward_mean": 200}, config={"env": "CartPole-v0", "num_workers":3, “monitor”: True})

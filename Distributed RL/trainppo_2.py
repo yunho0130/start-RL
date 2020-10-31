8985e89471e0c7b24ed63530feee63c649e3ba5e
Kyu Nam Cho <mystous@naver.com>
2020-10-31 23:20:41 +0900

    Create trainppo_2.py

---
+++ Distributed RL/trainppo_2.py
@@ -0,0 +1,7 @@
+# 파일이름을 trainppo.py로 변경 후 실행하세요.
+from ray import tune
+from ray.rllib.agents.ppo import PPOTrainer
+import ray
+
+ray.init(address="auto")
+tune.run(PPOTrainer, config={"env": "CartPole-v0", "num_workers":3})

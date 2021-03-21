e377714ae1c4e0120c55e02db481a8c73922552b
Kyu Nam Cho <mystous@naver.com>
2021-03-21 10:35:34 +0900

    Update trainppo_3.py

--- Distributed RL/trainppo_3.py
+++ Distributed RL/trainppo_3.py
@@ -4,4 +4,4 @@ from ray.rllib.agents.ppo import PPOTrainer
 import ray
 
 ray.init(address="auto")
-tune.run(PPOTrainer, stop={"episode_reward_mean": 200}, config={"env": "CartPole-v0", "num_workers":3, “monitor”: True})
+tune.run(PPOTrainer, stop={"episode_reward_mean": 200}, config={"env": "CartPole-v0", "num_workers":3, “monitor”: False})
